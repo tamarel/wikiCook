89f93892d6d0f20174f003feb7c0086742fa6272
babaniassaf@gmail.com <babaniassaf@gmail.com>
2015-06-02 18:20:21 +0300

    add master page

--- wiki-cook/web/pages/master_page.py
+++ wiki-cook/web/pages/master_page.py
@@ -7,13 +7,14 @@ import webapp2
 class IndexHandler(webapp2.RequestHandler):
 	def get(self):
 		template_params = {}
-#		user = User.checkUser()
-#		if not user:
-#			template_params['loginUrl'] = User.loginUrl()
-#		else:
-#			template_params['logoutUrl'] = User.logoutUrl()
-#			template_params['user'] = user.email
-	#			html = self.redirect('/salesEvents')
+		user = User.checkUser()
+		if not user:
+			self.redirect('/')
+			return
+		else:
+			template_params['logoutUrl'] = User.logoutUrl()
+			template_params['user'] = user.email
+
 
 		html = template.render("web/templates/master_page.html", template_params)
 		self.response.write(html)
