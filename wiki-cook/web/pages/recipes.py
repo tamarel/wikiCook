0e72a2bbb3a12614e278dd09aebb2e58020d1e6d
tamarel <tamareliyahou@gmail.com>
2015-06-08 13:44:22 +0300

    change simulator

--- wiki-cook/web/pages/recipes.py
+++ wiki-cook/web/pages/recipes.py
@@ -12,7 +12,8 @@ class IndexHandler(webapp2.RequestHandler):
 		template_params = {}
 		user = User.checkUser()
 		if not user:
-			template_params['loginUrl'] = User.loginUrl()
+			self.redirect('/')
+			return
 		else:
 			all_recipes = Recipe.getAllRecipe(user)
 			recipes=[]
